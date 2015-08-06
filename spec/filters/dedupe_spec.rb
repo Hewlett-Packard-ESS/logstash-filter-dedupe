3264f4a19a37be324799c0732821f0f6862180a7
Stono <karl@jambr.co.uk>
2015-08-06 21:48:36 +0100

    Flush redis before the tests

--- spec/filters/dedupe_spec.rb
+++ spec/filters/dedupe_spec.rb
@@ -1,7 +1,17 @@
 require 'logstash/devutils/rspec/spec_helper'
 require 'logstash/filters/dedupe'
+require 'redis'
 
+redis = Redis.new({
+  :host => 'redis',
+  :port => 6379
+})
+ 
 describe LogStash::Filters::DeDupe do
+
+  before(:all) do
+    redis.flushall
+  end
 
   describe 'Detects a duplicate' do
     config <<-CONFIG
